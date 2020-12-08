eb1fd653cf53751809415cef8151bfa03b0aa1da
Kevin Bastoul <kaycbas@gmail.com>
2020-12-08 13:54:57 -0800

    update application.rb to prevent precompile db connect

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Rocket
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # compile assets without your app attempting to connect to the database
+    # (For Heroku push issue)
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
