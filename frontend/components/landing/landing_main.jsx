212d2a38d017ac6ae0e8036b1be66b95c676a788
Kevin Bastoul <kaycbas@gmail.com>
2020-12-07 21:55:48 -0800

    landing page styling, in progress

--- frontend/components/landing/landing_main.jsx
+++ frontend/components/landing/landing_main.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LandingMain extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Main</h1>
-            </div>
+            <React.Fragment>
+                <div className='landing-main-container'>
+                    <section className="landing-panel">
+                        <div className="landing-panel-content">
+                            <h2>Put knowlege in your Pocket.</h2>
+                            <button className="email-signup-btn">Sign up with Email</button>
+                        </div>
+                    </section>
+                </div>
+                <div className='landing-main-container-2'>
+                    <section className="landing-panel-2">
+                        <div className="landing-panel-content-2">
+                            <h2>Save content from everywhere.</h2>
+                            <p>Save articles, videos and stories from any publication, page or app. Curate your own space filled with everything you can’t wait to learn.</p>
+                        </div>
+                        <div className="img-placeholder"></div>
+                    </section>
+                </div>
+            </React.Fragment>
         )
     }
 }
--- frontend/components/landing/landing_nav.jsx
+++ frontend/components/landing/landing_nav.jsx
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom';
 export default class LandingNav extends Component {
     render() {
         return (
-            <div>
-                <Link to='/'>Rocket 🚀</Link>
-                <Link to='/signup'>Sign Up</Link>
-                <Link to='/signin'>Log In</Link>
-            </div>
+            <header className="landing-header-container">
+                <div className="landing-header-content">
+                    <Link to='/' className="landing-logo">
+                        {/* <img src="assets/images/rocket_logo.png" alt="Couldn't load" /> */}
+                        <h1>🚀 rocket</h1>
+                    </Link>
+                    <ul className="minimal-header">
+                        <Link to='/signup'>
+                            <button className="landing-signup-btn">Sign Up</button>
+                        </Link>
+                        <Link to='/signin' className="btn">
+                            <button className="landing-signin-btn">Log In</button>
+                        </Link>
+                    </ul>
+                </div>
+            </header>
         )
     }
 }
