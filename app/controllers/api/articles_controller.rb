0742a58e8ed6bb335325bc58ddad0d3ec1fa9b08
Kevin Bastoul <kaycbas@gmail.com>
2020-12-09 17:19:33 -0800

    add articles controller and routes

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,23 @@
+class Api::ArticlesController < ApplicationController
+    def index
+        
+    end
+
+    def show
+        
+    end
+
+    def create
+        
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def article_params
+        
+    end
+end
