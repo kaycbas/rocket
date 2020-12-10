28ae124f61ebb1b7045c24ea865b2eb5f249febd
Kevin Bastoul <kaycbas@gmail.com>
2020-12-10 15:43:26 -0800

    tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # config/environments/production.rb
-  config.active_storage.service = :amazon_dev
+  # config.active_storage.service = :amazon_dev
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  # config.active_storage.service = :local
+  config.active_storage.service = :amazon_prod
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
