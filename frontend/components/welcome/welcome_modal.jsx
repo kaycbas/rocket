bbdf5e81c2615d61601e6748c222c0cf89198256
Kevin Bastoul <kaycbas@gmail.com>
2021-02-05 18:09:54 -0800

    add top and bottom border

--- frontend/components/welcome/welcome_modal.jsx
+++ frontend/components/welcome/welcome_modal.jsx
@@ -5,6 +5,7 @@ function WelcomeModal() {
     return (
         <div className="welcome-background">
             <div className="welcome-modal">
+                {/* <div className="welcome-border-top"></div> */}
                 <span className="close-modal-btn">&#x2715;</span>
                 <h1>Welcome to Rocket!</h1>
                 <p>Simply, Rocket lets you save articles for later reading.</p>
@@ -13,6 +14,7 @@ function WelcomeModal() {
                 <p>• Featured articles can be found and saved from the <span>Discover</span> tab</p>
                 <p>• Articles can be added by clicking the <span>+</span> icon in the navbar and pasting in an article url (try an article from Medium!)</p>
                 <p>• Saved articles show up in the <span>My List</span> tab. From here you can favorite, tag, archive, or delete your saved articles.</p>
+                {/* <div className="welcome-border-bottom"></div> */}
             </div>
         </div>
     );
