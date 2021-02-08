24342810f4db363921d65fb4366792bffd3b9042
Kevin Bastoul <kaycbas@gmail.com>
2021-02-08 13:31:12 -0800

    update welcome modal

--- frontend/components/welcome/welcome_modal.jsx
+++ frontend/components/welcome/welcome_modal.jsx
@@ -8,12 +8,13 @@ function WelcomeModal({ openModal, closeModal }) {
                 {/* <div className="welcome-border-top"></div> */}
                 <span onClick={() => closeModal()} className="close-modal-btn">&#x2715;</span>
                 <h1>Welcome to Rocket!</h1>
+                <h2>What is it?</h2>
                 <p>Simply, Rocket lets you save articles for later reading.</p>
                 <p>But it's also a powerful tool to organize and track your consumption of content from around the internet.</p>
-                <h1>Some tips:</h1>
-                <p>• Featured articles can be found and saved from the <span>Discover</span> tab</p>
-                <p>• Articles can also be saved by clicking the <span>+</span> icon in the navbar and pasting in an article URL (try an article from Medium!)</p>
-                <p>• Saved articles show up in the <span>My List</span> tab. From here you can favorite, tag, archive, or delete your saved articles.</p>
+                <h2>Some tips:</h2>
+                <p>• Featured articles can be found and saved from the <span className='highlight-1'>Discover</span> tab</p>
+                <p>• Articles can also be saved by clicking the <span className='highlight-2'>+</span> icon in the navbar and pasting in an article URL (try an article from Medium!)</p>
+                <p>• Saved articles show up in the <span className='highlight-3'>My List</span> tab. From here you can favorite, tag, archive, or delete your saved articles.</p>
                 {/* <div className="welcome-border-bottom"></div> */}
             </div>
         </div>
